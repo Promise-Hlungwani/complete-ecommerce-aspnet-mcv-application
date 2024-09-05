aef19b5f131e9f8be2c8d4ae8ea1c57cfb0638fa
Promise <shitshembiso0@gmail.com>
2024-09-05 13:43:52 +0200

    Adding migration

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -6,12 +6,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+
+
 
 // Add DbContext with SQL Server and connection string from configuration
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionStrings"))
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"))
 );
+
+// Build the app
+var app = builder.Build();
+
+
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
